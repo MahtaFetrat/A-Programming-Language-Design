e9562efe1babaca09d546769391d7e9479cebe6a
MahtaFetrat <62302965+MahtaFetrat@users.noreply.github.com>
2021-07-11 17:29:30 +0430

    implement lexer

--- src/lexer.rkt
+++ src/lexer.rkt
@@ -1,43 +1,61 @@
 #lang racket
+(require parser-tools/lex
+         (prefix-in : parser-tools/lex-sre))
 
-"
-to be lexed:
+(provide lex)
 
-`;`
-`pass‘
-‘break‘
-‘continue`
- =
-`return`
-`global`
-‘def‘
-‘(‘
-‘)‘
-‘ : ‘
-‘, ‘
-‘if‘
-‘else‘
-‘for‘
-‘in‘
-‘or‘
-‘and‘
-‘not‘
-‘ == ‘
-‘ < ‘
-‘ > ‘
-‘ + ‘
-‘ - ‘
-‘ ∗ ‘
-‘/‘
-‘ ∗ ∗‘
-‘[‘
-‘]‘
-‘()‘
-‘True‘
-‘False‘
-‘None‘
-‘[]‘
-NUMBER
-ID
-EOF
-"
+(define-tokens a (NUM ID))
+(define-empty-tokens b (SEMICOL PASS BREAK CONTINUE ASSIGN RETURN
+                                GLOBAL DEF OPEN-PAR CLOSE-PAR COLON
+                                COMMA IF ELSE FOR IN OR AND NOT EQUAL
+                                LESS GREATER PLUS MINUS MUL POW DIV
+                                OPEN-BRACKET CLOSE-BRACKET ZERO-ARG
+                                TRUE FALSE NONE EMPTY-LIST EOF))
+(define-lex-abbrev :? (repetition 0 1))
+
+(define py-lexer
+  (lexer
+   (";" (token-SEMICOL))
+   ("pass" (token-PASS))
+   ("break" (token-BREAK))
+   (whitespace (py-lexer input-port))
+   ((eof) (token-EOF))
+   ("continue" (token-CONTINUE))
+   ("=" (token-ASSIGN))
+   ("return" (token-RETURN))
+   ("global" (token-GLOBAL))
+   ("def" (token-DEF))
+   ("(" (token-OPEN-PAR))
+   (")" (token-CLOSE-PAR))
+   (":" (token-COLON))
+   ("," (token-COMMA))
+   ("if" (token-IF))
+   ("else" (token-ELSE))
+   ("for" (token-FOR))
+   ("in" (token-IN))
+   ("or" (token-OR))
+   ("and" (token-AND))
+   ("not" (token-NOT))
+   ("==" (token-EQUAL))
+   ("<" (token-LESS))
+   (">" (token-GREATER))
+   ("+" (token-PLUS))
+   ("-" (token-MINUS))
+   ("*" (token-MUL))
+   ("**" (token-POW))
+   ("/" (token-DIV))
+   ("[" (token-OPEN-BRACKET))
+   ("]" (token-CLOSE-BRACKET))
+   ("()" (token-ZERO-ARG))
+   ("True" (token-TRUE))
+   ("False" (token-FALSE))
+   ("None" (token-NONE))
+   ("[]" (token-EMPTY-LIST))
+   ((:or
+     (:+ (char-range #\0 #\9))
+     (:: (:+ (char-range #\0 #\9)) #\. (:+ (char-range #\0 #\9))))
+    (token-NUM (string->number lexeme)))
+   ((:: (:or (char-range #\A #\Z) (char-range #\a #\z) #\_) (:* (:or (char-range #\A #\Z) (char-range #\a #\z) #\_ (char-range #\0 #\9))))
+    (token-ID (string->symbol lexeme)))))
+   
+(define lex (lambda (input) (lambda () (py-lexer input))))
