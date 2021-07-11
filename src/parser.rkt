dd8c11f94b553fb39db01310ecb3037889ea5400
MahtaFetrat <62302965+MahtaFetrat@users.noreply.github.com>
2021-07-11 19:24:07 +0430

    add parser for grammar 1-11

--- src/parser.rkt
+++ src/parser.rkt
@@ -1,36 +1,49 @@
 #lang racket
 
-"
-based on lexer information, one of the grammar types in 'grammar.rkt' will be created
+(require parser-tools/yacc)
 
-for more detail: see the the sample parser provided in the doc:
-...
-(define simple-math-parser
-           (parser
-            (start exp)
-            (end EOF)
-            (error void)
-            (tokens a b)
-            (grammar
-           (exp ((exp plus NUM) (list 'plusnumbers $1 $3)) ((NUM) (list 'anumber $1)))
-             )))
-...
-the returning result of the 'simple-math-parser' is a list of required values. Instead, it could be an eopl datatype.
+(require "lexer.rkt"
+         "grammar.rkt")
 
-e.g:
-...
-(define simple-math-parser
-           (parser
-            (start exp)
-            (end EOF)
-            (error void)
-            (tokens a b)
-            (grammar
-           (exp ((exp plus NUM) (plusnumbers $1 $3)) ((NUM) (a-number $1)))
-             )))
-...
-where:
-(define-datatype anumber anumber?
-     (a-number
-          (num number?)))
-and plusnumber is another datatype like this"
+(define py-parser
+  (parser
+   (start program)
+   (end EOF)
+   (error (lambda (tok-ok? tok-name tok-value) (error 'parser "problem parsing '~s ~s'" tok-name to-value)))
+   (tokens a b)
+   (grammar
+    (program
+     ((statements EOF) (a-program $1)))
+    (statements
+     ((statement SEMICOL) (single-statement $1))
+     ((statements statements SEMICOL) (mult-statement $1 $2)))
+    (statement
+     ((simple-stmt) (a-single-stmt $1))
+     ((compound-stmt) (a-compound-stmt $1)))
+    (simple-stmt
+     ((assignment) (assignment-simple-stmt $1))
+     ((return-stmt) (return-simple-stmt $1))
+     ((global-stmt) (global-simple-stmt $1))
+     ((PASS) (pass-stmt))
+     ((CONTINUE) (continue-stmt))
+     ((BREAK) (break-stmt)))
+    (compound-stmt
+     ((function-def) (func-def-comp-stmt $1))
+     ((if-stmt) (if-comp-stmt $1))
+     ((for-stmt) (for-comp-stmt $1)))
+    (assignment
+     ((ID ASSIGN expression) (an-assignment $1 $3)))
+    (return-stmt
+     ((RETURN) (empty-return-stmt))
+     ((RETURN expression) (exp-return-stmt $2)))
+    (global-stmt
+     ((GLOBAL ID) (a-global-stmt $2)))
+    (function-def
+     ((DEF ID OPEN-PAR params CLOSE-PAR COLON statements) (params-func-def $2 $4 $7))
+     ((DEF ID ZERO-ARG COLON statements) (zero-param-func-def $2 $5)))
+    (params
+     ((param-with-defaulta) (single-param $1))
+     ((params COMMA params-with-default) (mult-param $1 $3)))
+    (param-with-default
+     ((ID ASSIGN expression) (a-param-with-default $1 $3))))))
+    
