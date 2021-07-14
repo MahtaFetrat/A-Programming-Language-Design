2e3ae5e5e24ffb9c59f9c00954e5398467367587
MahtaFetrat <62302965+MahtaFetrat@users.noreply.github.com>
2021-07-14 23:41:09 +0430

    add comparison answer datatype

--- src/answer.rkt
+++ src/answer.rkt
@@ -1,6 +1,7 @@
 #lang eopl
 
-(require "environment.rkt")
+(require "environment.rkt"
+         "grammar.rkt")
 
 (define expval?
   (lambda (e) (or (number? e) (boolean? e) (null? e) (py-list? e))))
@@ -36,3 +37,22 @@
     (cases answer ans
       (an-answer (val msg sc) (eqv? msg 'break)))))
 
+
+;comparison answer ---------------------------------------------------------------
+(define-datatype cmp-answer cmp-answer?
+  (a-cmp-answer
+   (result boolean?)
+   (right-hand-operand atom?)))
+
+(define cmp-res
+  (lambda (cmp-ans)
+    (cases cmp-answer cmp-ans
+      (a-cmp-ans (res rh) res))))
+
+(define cmp-right-hand-operand
+  (lambda (cmp-ans)
+    (lambda (cmp-ans)
+      (cases cmp-answer cmp-ans
+        (a-cmp-ans (res rh) rh)))))
+    
+   
