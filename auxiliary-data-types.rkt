f98efc1acaa173d6cad121aec9d4b7968369d26b
MahtaFetrat <62302965+MahtaFetrat@users.noreply.github.com>
2021-07-15 17:36:20 +0430

    add evaluated list datatype

--- auxiliary-data-types.rkt
+++ auxiliary-data-types.rkt
@@ -5,7 +5,14 @@
 
 (provide (all-defined-out))
 
+;thunk datatype
 (define-datatype thunk thunk?
   (a-thunk
    (exp expression?)
+   (scope scope?)))
+
+;evaluated list datatype
+(define-datatype eval-list eval-list?
+  (an-eval-list
+   (py-list py-list?)
    (scope scope?)))
